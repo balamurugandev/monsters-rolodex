e814cac7daacbb6a7d2ba7542099d83343e78ddf
Balamurugan S <balamurugans@Balamurugans-Mac-mini.local>
2023-03-11 19:51:37 +0530

    thrid commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import {Component} from 'react'; 
+import './App.css';
+
+class App extends Component {
+ constructor(){
+  super();
+  
+  this.state = {
+    monsters: [],
+  };
+  console.log('constructor')
+ }
+
+ componentDidMount(){
+  console.log('componentDidMount')
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(response => response.json())
+   .then((users) => this.setState(() => {
+   return{monsters: users}
+ },
+ () => {
+  console.log(this.state);
+ }));
+ }
+
+  render(){
+    console.log('render')
+  return (
+    <div className="App">
+      <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+      const searchString = event.target.value.toLocaleLowerCase(); 
+      const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchString);
+      })
+      this.setState(() => {
+        return { monsters: filteredMonsters}
+      })
+
+
+      }}/>
+    {
+      this.state.monsters.map((monster)=> {
+        return <div key={monster.id}><h1>{monster.name}</h1></div>;
+      })                                                                                                                       
+    }  
+    </div>
+  );
+  }
+}
+
+export default App;
