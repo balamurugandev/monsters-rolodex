ea1e93ef5222c5021eafaf8d4319b35e299de365
Balamurugan S <balamurugans@Balamurugans-Mac-mini.local>
2023-03-25 16:50:19 +0530

    added search-box css

--- src/App.js
+++ src/App.js
@@ -40,9 +40,9 @@ class App extends Component {
   return (
     <div className="App">
          <SearchBox 
-         className = 'search-box'
+         className = 'monsters-search-box'
          onChangeHandler={onSearchChange} 
-         placeholder='search monsters'/>
+         placeholder= 'search monsters'/>
          <CardList monsters={filteredMonsters}/>
     </div>
     );
