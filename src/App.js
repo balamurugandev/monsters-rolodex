8fc4fca8acb470b1075f73692e837570c3da5956
Balamurugan S <balamurugans@Balamurugans-Mac-mini.local>
2023-03-26 20:47:46 +0530

    build with class comp

--- src/App.js
+++ src/App.js
@@ -39,6 +39,8 @@ class App extends Component {
     });
   return (
     <div className="App">
+         <h1 className='app-title'>Monsters Rolodex</h1>
+
          <SearchBox 
          className = 'monsters-search-box'
          onChangeHandler={onSearchChange} 
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import './card-list.styles.css';
+import Card from '../card/card.component'
 
 class CardList extends Component {
     render() {
@@ -9,17 +10,13 @@ class CardList extends Component {
  return(
         <div className="card-list">
         {monsters.map((monster) => {
-          const {name,email,id} = monster;
+    
           return(
-           <div className="card-container" key={id}>
-           <img alt={`monster ${name}`} 
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
-          <h2>{name}</h2>
-          <p>{email}</p>
-          </div>
-)})}        
-      </div> 
-      );
+         <Card monster ={monster} />
+         );
+        })}        
+         </div> 
+        );
    }
 }
 
