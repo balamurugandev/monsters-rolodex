a506986548fdf46372bab205d5dd6e2ce70d7667
Balamurugan S <balamurugans@Balamurugans-Mac-mini.local>
2023-03-27 00:19:05 +0530

    converted app.js file
    converted app.js from class to functional component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
-import {Component} from 'react'; 
+import { useState, useEffect } from 'react';
 import CardList  from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
- constructor(){
-  super();
-  
-  this.state = {
-    monsters: [],
-    searchField: '',
-  };
- }
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
- componentDidMount(){
-   fetch('https://jsonplaceholder.typicode.com/users')
-   .then(response => response.json())
-   .then((users) => this.setState(() => {
-   return{monsters: users};
- },
-));
- }
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users) => setMonsters(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase(); 
-    this.setState(() => {
-      return { searchField };
-    });
-    }
-
-  render(){
-
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFiltereMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
-  return (
+
+    setFilteredMonsters(newFiltereMonsters);
+  },[monsters, searchField]);
+     
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase(); 
+    setSearchField(searchFieldString);
+    }
+
+  return(
     <div className="App">
          <h1 className='app-title'>Monsters Rolodex</h1>
 
@@ -47,8 +37,10 @@ class App extends Component {
          placeholder= 'search monsters'/>
          <CardList monsters={filteredMonsters}/>
     </div>
-    );
-  }
-}
+
+  )
+};
+
+
 
 export default App;
